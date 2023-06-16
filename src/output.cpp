7d77147af73395b5f3af1763b7c8599e89566d86
Eugene Kopilov <64531188+Evgenk2020@users.noreply.github.com>
2023-06-16 18:28:49 +0300

    Add files via upload

--- src/output.cpp
+++ src/output.cpp
@@ -51,7 +51,6 @@ void screen_info::see_info(wort_solution *wrt)
 void file_info::see_info(wort_solution *wrt)
 {
     std::ofstream writer("wort-dada.csv", std::ios::app);
-
     const char coma = ',';
     const char quo = '\"';
 
