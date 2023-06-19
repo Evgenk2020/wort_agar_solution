32b1009c48e1a7a8683c412611d8f9d7359f16e7
Eugene Kopilov <64531188+Evgenk2020@users.noreply.github.com>
2023-06-19 12:10:09 +0300

    Add files via upload

--- src/output.cpp
+++ src/output.cpp
@@ -7,30 +7,30 @@ general_info::~general_info() {}
 
 void help_info::see_info()
 {
-    std::cout << "*** Определение объема компонентов сусло-агара ***" << std::endl
+    std::cout << "*** Визначення об'єму компонентів суслового агару з натуральної сировини ***" << std::endl
               << std::endl
-              << "Для вывода в терминал" << std::endl
-              << "wort -d [начальная концентация] [необходимая концентрация] [объем фильтара]" << std::endl
-              << "Для вывода в файл" << std::endl
-              << "wort -df [начальная концентация] [необходимая концентрация] [объем фильтара]" << std::endl
-              << "Для вывода справки" << std::endl
+              << "Для виводу в термінал" << std::endl
+              << "wort -d [початкова_концентрація %] [кінцева_концентрація %] [об'єм_фільтрату мл]" << std::endl
+              << "Для виводу у файл" << std::endl
+              << "wort -df [початкова_концентрація %] [кінцева_концентрація %] [об'єм_фільтрату мл]" << std::endl
+              << "Для довідки" << std::endl
               << "wort [-h|--help]" << std::endl
-              << "Для просмотра дополнительной информации" << std::endl
+              << "Додаткова інформація" << std::endl
               << "wort -i" << std::endl;
 }
 
 void inf_indo::see_info()
 {
-    std::cout << "В лабораторных условиях приготовление растворов с заданной" << std::endl
-              << "массовой долей растворенного вещества из растворов" << std::endl
-              << "с исходной массовой долей осуществоляется в соответствии с \"правилом креста\":" << std::endl
+    std::cout << "В лабораторних умовах приготування розчину заданої" << std::endl
+              << "масової чатки розчиненої речовини з розчинів" << std::endl
+              << "з відомою масовою часткою здійснюється відповідно правилу хреста:" << std::endl
               << "m1 * W1 + m2 * W2 = W3 * (m1 + m2)" << std::endl
               << std::endl
-              << "Утилита определяет данные:" << std::endl
-              << "- объем воды для разбавления концентрированного раствора" << std::endl
-              << "- объем разбавленного раствора" << std::endl
-              << "исходя из концентрации начального раствора, объема фильтрата" << std::endl
-              << "и концентрации приготавливаемого раствора" << std::endl;
+              << "Утиліта визначає дані:" << std::endl
+              << "- об'єм води для розведення концентрованого розчину" << std::endl
+              << "- об'єм розведеного розчину" << std::endl
+              << "виходячи з концентрації початкового розчину, об'єму фільтрата" << std::endl
+              << "і концентрації розчину, який готується" << std::endl;
 }
 
 //--------------------------------------------------
@@ -39,13 +39,13 @@ data_info::~data_info() {}
 
 void screen_info::see_info(wort_solution *wrt)
 {
-    std::cout << "Концентрация неразбавленного раствора: " << wrt->first_wort << " %" << std::endl;
-    std::cout << "Концентрация разбавленного раствора: " << wrt->finish_wort << " %" << std::endl;
-    std::cout << "Объем фильтрата: " << wrt->vol_filtrate << " мл" << std::endl;
+    std::cout << "Концентрація нерозведеного розчина: " << wrt->first_wort << " %" << std::endl;
+    std::cout << "Концентрація розведеного розчина: " << wrt->finish_wort << " %" << std::endl;
+    std::cout << "Об'єм фільтрата: " << wrt->vol_filtrate << " мл" << std::endl;
 
     solution sol;
-    std::cout << "Объем воды для разбавления: " << sol.solutions(sol.water_for_solvation)->get_solvation(*wrt) << " мл" << std::endl;
-    std::cout << "Объем разбавленной среды: " << sol.solutions(sol.total_volume)->get_solvation(*wrt) << " мл" << std::endl;
+    std::cout << "Об'єм води для розчинення: " << sol.solutions(sol.water_for_solvation)->get_solvation(*wrt) << " мл" << std::endl;
+    std::cout << "Об'єм розчиненого середовища: " << sol.solutions(sol.total_volume)->get_solvation(*wrt) << " мл" << std::endl;
 }
 
 void file_info::see_info(wort_solution *wrt)
@@ -57,16 +57,17 @@ void file_info::see_info(wort_solution *wrt)
     std::locale m_loc("uk_UA.utf8");
     writer.imbue(m_loc);
 
-    writer << "Концентрация разбавленного раствора:" << coma << quo << wrt->finish_wort << quo << coma << "%" << std::endl;
-    writer << "Объем фильтрата:" << coma << quo << wrt->vol_filtrate << quo << coma << "мл" << std::endl;
+    writer << "Концентрація нерозведеного розчина:" << coma << quo << wrt->first_wort << quo << coma << "%" << std::endl;
+    writer << "Концентрація розведеного розчина:" << coma << quo << wrt->finish_wort << quo << coma << "%" << std::endl;
+    writer << "Об'єм фільтрата:" << coma << quo << wrt->vol_filtrate << quo << coma << "мл" << std::endl;
 
     solution sol;
 
-    writer << "Объем воды для разбавления:" << coma << quo << sol.solutions(sol.water_for_solvation)->get_solvation(*wrt) << quo << coma << "мл" << std::endl;
-    writer << "Объем разбавленной среды:" << coma << quo << sol.solutions(sol.total_volume)->get_solvation(*wrt) << quo << coma << "мл" << std::endl;
+    writer << "Об'єм води для розчинення:" << coma << quo << sol.solutions(sol.water_for_solvation)->get_solvation(*wrt) << quo << coma << "мл" << std::endl;
+    writer << "Об'єм розчиненого середовища:" << coma << quo << sol.solutions(sol.total_volume)->get_solvation(*wrt) << quo << coma << "мл" << std::endl;
     writer << "" << std::endl;
 
-    std::cout << "Данные добавлены в файл wort-dada.csv" << std::endl;
+    std::cout << "Дані додані в файл wort-dada.csv" << std::endl;
 }
 
 //--------------------------------------------------
