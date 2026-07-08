80ba0fbb0edec779835823b890ec2763ea8f9195
evheny <evhenykopilov@ukr.net>
2026-07-08 16:31:38 +0300

    modified 	modified:   CMakeLists.txt 	modified:   build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-curr=$(dirname $(realpath $0))
-bld_target=$curr/build
+set -e
 
-#rm -rd $bld_target
+curr="$(cd "$(dirname "$0")" && pwd)"
+bld_target="$curr/build"
 
-if [ -d $bld_target ]
-then
-    cd $bld_target
-else
-    mkdir $bld_target
-    cd $bld_target
-fi
+mkdir -p "$bld_target"
+cd "$bld_target"
 
 cmake ..
-make
+cmake --build . --parallel
 cpack
-
-exec $SHELL
